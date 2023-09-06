f4393f7bad29de3e5075e68a9dce3586c41eb228
toadsam <toadsam@naver.com>
2023-09-06 15:04:36 +0900

    [ADD] 캐릭터 선택 (-재훈)
    캐릭터 선택창 추가

--- Assets/Scripts/Main Camera.cs
+++ Assets/Scripts/Main Camera.cs
@@ -4,17 +4,21 @@ using UnityEngine;
 
 public class MainCamera : MonoBehaviour
 {
-    public Transform player;
+    // public Transform player;
+    public GameObject player;
+    public ReSpawn reSpawn;
     // Start is called before the first frame update
     void Start()
     {
-        
+       
+      // player = Instantiate(reSpawn.cahrPrefabs[(int)DataMgr.Instance.currentCharacter]);
     }
 
     // Update is called once per frame
     void LateUpdate()
     {
-        transform.position = Vector3.Lerp(transform.position, player.position, Time.deltaTime);
+        player = reSpawn.player;
+        transform.position = Vector3.Lerp(transform.position, player.transform.position, Time.deltaTime);
         transform.position = new Vector3(transform.position.x, transform.position.y, -10f);
     }
 }
