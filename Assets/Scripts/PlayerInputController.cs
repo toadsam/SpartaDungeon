9a28f7fff9103fab56e5fc1d19d4e2f6dc234e32
toadsam <toadsam@naver.com>
2023-09-07 19:06:37 +0900

    [ADD]  캐릭터 이름 (-재훈)
    이름텍스트 캐릭터 따라다니기 구현

--- Assets/Scripts/PlayerInputController.cs
+++ Assets/Scripts/PlayerInputController.cs
@@ -8,22 +8,22 @@ public class PlayerInputController : TopDownCharacterController
     private Camera _camera;
     private void Awake()
     {
-        _camera = Camera.main;
+        _camera = Camera.main;  //카메라는 메인 카메라
     }
 
-    public void OnMove(InputValue value)
+    public void OnMove(InputValue value)  //매게 변수를 InputValue형태로 받기
     {
         // Debug.Log("OnMove" + value.ToString());
-        Vector2 moveInput = value.Get<Vector2>().normalized;
-        CallMoveEvent(moveInput);
+        Vector2 moveInput = value.Get<Vector2>().normalized;  //움직이는 방향의 백테값 얻기
+        CallMoveEvent(moveInput);   //CallMoveEvent(moveInput)실행
     }
 
     public void OnLook(InputValue value)
     {
         // Debug.Log("OnLook" + value.ToString());
-        Vector2 newAim = value.Get<Vector2>();
-        Vector2 worldPos = _camera.ScreenToWorldPoint(newAim);
-        newAim = (worldPos - (Vector2)transform.position).normalized;
+        Vector2 newAim = value.Get<Vector2>();  //입력받은 벡터값
+        Vector2 worldPos = _camera.ScreenToWorldPoint(newAim);  //위치는 카메라 범위안의 백터 값을 넣기 월드 스페이스로 반환해줌
+        newAim = (worldPos - (Vector2)transform.position).normalized;  
 
         if (newAim.magnitude >= .9f)
         {
